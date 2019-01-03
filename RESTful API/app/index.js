833cd21b088d98d6a0d1bb5803ff9a75a8c5ad3d
Jordan Blakey <jordan@pulsem.me>
2019-01-02 22:02:29 -0600

    Created helper to manage sending Twilio SMS messages

--- RESTful API/app/index.js
+++ RESTful API/app/index.js
@@ -3,14 +3,19 @@
  */
 
 // Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
+var config = require("./lib/config");
+var fs = require("fs");
+var handlers = require("./lib/handlers");
+var helpers = require("./lib/helpers");
+
+// Test Twilio SMS Sending
+// helpers.sendTwilioSms("4698595332", "Ahoy from Twilio", function(err) {
+//   console.log("this was the error:", err);
+// });
 
 // Instantiating the HTTP server
 var httpServer = http.createServer(function(req, res) {
@@ -19,13 +24,13 @@ var httpServer = http.createServer(function(req, res) {
 
 // Start the server
 httpServer.listen(config.httpPort, function() {
-  console.log('The server is listening on port ' + config.httpPort + ' in ' + config.envName + ' mode...');
+  console.log("The server is listening on port " + config.httpPort + " in " + config.envName + " mode...");
 });
 
 // Instantiate the HTTPS server
 var httpsServerOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem')
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem")
 };
 
 var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
@@ -34,7 +39,7 @@ var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
 
 // Start the HTTPS server
 httpsServer.listen(config.httpsPort, function() {
-  console.log('The server is listening on port ' + config.httpsPort + ' in ' + config.envName + ' mode...');
+  console.log("The server is listening on port " + config.httpsPort + " in " + config.envName + " mode...");
 });
 
 // All the server logic for port the http and https server
@@ -44,7 +49,7 @@ var unifiedServer = function(req, res) {
 
   // Get the path
   var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // Get the query string as and object
   var queryStringObject = parsedUrl.query;
@@ -56,16 +61,16 @@ var unifiedServer = function(req, res) {
   var headers = req.headers;
 
   // Get the payload, if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', function(data) {
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
     buffer += decoder.write(data);
   });
-  req.on('end', function() {
+  req.on("end", function() {
     buffer += decoder.end();
 
     // Choose the handler this request should go to. If one is not found, go to the notFound handler
-    var chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    var chosenHandler = typeof router[trimmedPath] !== "undefined" ? router[trimmedPath] : handlers.notFound;
 
     // Construct the data object to send to the handler
     var data = {
@@ -78,26 +83,26 @@ var unifiedServer = function(req, res) {
 
     chosenHandler(data, function(statusCode, payload) {
       // Use the status code called back by the handler or default to 200
-      statusCode = typeof statusCode == 'number' ? statusCode : 200;
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
       // Use the payload called back by the handler, or default to an empty object
-      payload = typeof payload == 'object' ? payload : {};
+      payload = typeof payload == "object" ? payload : {};
 
       // Convert the payload to a string
       var payloadString = JSON.stringify(payload);
 
       // Return the response
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(statusCode);
       res.end(payloadString);
 
       // Log the request path
-      console.log('Returning this response:', statusCode, payloadString);
+      console.log("Returning this response:", statusCode, payloadString);
     });
 
     // Log the request path
-    console.log('Request received on path: ' + trimmedPath + ' with method ' + method + ' and with these query string parameters:', queryStringObject);
+    console.log("Request received on path: " + trimmedPath + " with method " + method + " and with these query string parameters:", queryStringObject);
     // console.log('Headers for the request:', headers);
-    console.log('Payload for the request:', buffer);
+    console.log("Payload for the request:", buffer);
   });
 };
 
--- RESTful API/app/lib/config.js
+++ RESTful API/app/lib/config.js
@@ -9,25 +9,35 @@ var environments = {};
 environments.staging = {
   httpPort: 3000,
   httpsPort: 3001,
-  envName: 'staging',
-  hashingSecret: 'thisIsASecret',
-  maxChecks: 5
+  envName: "staging",
+  hashingSecret: "thisIsASecret",
+  maxChecks: 5,
+  twilio: {
+    accountSid: "AC3aa1b8e39dc68e04540122530c0d344c",
+    authToken: "e0a671397f473956e3870419b322a61f",
+    fromPhone: "+14695572603"
+  }
 };
 
 // Production environment
 environments.production = {
   httpPort: 5000,
   httpsPort: 5001,
-  envName: 'production',
-  hashingSecret: 'thisIsAlsoASecret',
-  maxChecks: 5
+  envName: "production",
+  hashingSecret: "thisIsAlsoASecret",
+  maxChecks: 5,
+  twilio: {
+    accountSid: "AC3aa1b8e39dc68e04540122530c0d344c",
+    authToken: "e0a671397f473956e3870419b322a61f",
+    fromPhone: "+14695572603"
+  }
 };
 
 // Determine which environment was passed as a command-line argument
-var currentEnvironment = typeof process.env.NODE_ENV == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+var currentEnvironment = typeof process.env.NODE_ENV == "string" ? process.env.NODE_ENV.toLowerCase() : "";
 
 // Check that the current environment is one of the environments above, if not, default to staging
-var environmentToExport = typeof environments[currentEnvironment] == 'object' ? environments[currentEnvironment] : environments.staging;
+var environmentToExport = typeof environments[currentEnvironment] == "object" ? environments[currentEnvironment] : environments.staging;
 
 // Export the module
 module.exports = environmentToExport;
