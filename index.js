02e9ab63fc6fd9897ed78504f81a372e4a6853b7
Jordan Blakey <jordan@pulsem.me>
2019-02-05 21:12:47 -0600

    CLI basic scaffolding using readline

--- index.js
+++ index.js
@@ -3,23 +3,29 @@
  */
 
 //  Dependencies
-var server = require('./lib/server');
-var workers = require('./lib/workers');
+var server = require('./lib/server')
+var workers = require('./lib/workers')
+var cli = require('./lib/cli')
 
 // Declare the app
-var app = {};
+var app = {}
 
 // Init function
 app.init = function() {
-  // Start the server
-  server.init();
+    // Start the server
+    server.init()
 
-  // Start the workers
-  workers.init();
-};
+    // Start the workers
+    workers.init()
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(function() {
+        cli.init()
+    }, 50)
+}
 
 // Execute
-app.init();
+app.init()
 
 // Export the app
-module.exports = app;
+module.exports = app
